480aed4ad7a8c67b73eeeef33e5c83c15f22cb11
sakethdosapati <sakethdosapati07@gmail.com>
2024-10-05 08:58:07 +0530

    wheather project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css";
 
-function App() {
+const App = () => {
+  let api = {
+    key: "54331c433e226b0bd0d4c7be4d41b3b4",
+    url: "https://api.openweathermap.org/data/2.5/weather"
+  };
+
+  let [search, setSearch] = useState("");
+  let [weather, setWeather] = useState({});
+  let [found, setFound] = useState("");
+  let [isloading, setIsLoading] = useState(false);
+
+  function searchCity() {
+    setIsLoading(true);
+    fetch(`${api.url}?q=${search}&appid=${api.key}&units=metric`)
+      .then(res => res.json())
+      .then(val => {
+        setWeather(val)
+        if(val.name === undefined){
+          setFound("City not found.Enter valid city name");
+        } else{
+          setFound("");
+        }
+        setIsLoading(false);
+      }).catch(()=>{
+        setSearch("Error in loading weather")
+        setIsLoading(false);
+      } )
+      
+  }
+
+  function keyPressEnter(e) {
+    if (e.key === 'Enter') {
+      searchCity();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <h1>Weather App</h1>
+      <input
+        placeholder='Enter the City'
+        type='text'
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyUp={keyPressEnter} 
+      />
+      <button onClick={searchCity}>Search</button>
+      <div>
+        {isloading? "Loading..." : found}
+        {weather.name && (
+          <div id='result'>
+            <p>Name: <strong>{weather.name}</strong></p>
+            <p>Temperature: <strong>{weather.main.temp} °C</strong> </p>
+          </div>
+        )}
+      </div>
     </div>
+   
   );
 }
 
